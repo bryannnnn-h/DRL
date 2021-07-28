c01f89c5456fe530d8eaa74c299ffde3daaddfac
TimOAO <t072000@gmail.com>
2021-07-29 07:04:44 +0800

    add tic_set in config

--- DRL-Trading-master/config/config.py
+++ DRL-Trading-master/config/config.py
@@ -19,7 +19,7 @@ VALID_START = "2016-01-01"   # date starting validation
 VALID_END = "2020-12-01"   # date ending validation
 OBS_DIM = 61   # dimension of state representation (select 10 of 0050 constituent stocks)
 
-
+tic_set = [1301,1303,2303,2308,2317,2330,2454,2882,2891,3008]
 
 #VIX
 vix_mean=18.1
